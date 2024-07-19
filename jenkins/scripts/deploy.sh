e7eb34f1e2f68f1c6685ca55e3567c103c49eec0
XandeK <46686422+XandeK@users.noreply.github.com>
2024-07-19 17:41:40 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Temp\\ICT2216-SSD\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+#docker run -d -p 80:80 --name my-apache-php-app -v C:\\Temp\\ICT2216-SSD\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /Users/xandekoh/Documents/GitHub/jenkins-php-selenium-test//src:/var/www/html php:7.2-apache
+
 sleep 1
 set +x
 
